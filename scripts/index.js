2fc6db6fc5f4655e3688bbc3e98439d8178f8c7f
Egor Sibirev <lay.12345@yandex.ru>
2022-05-22 12:52:44 +0300

    подправить index.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-button');
+const nameInput = document.querySelector('.popup__name');
+const jobInput = document.querySelector('.popup__job');
+const jobInputNew = document.querySelector('.profile__description');
+const nameInputNew = document.querySelector('.profile__name');
+const formElement = document.querySelector('.popup__container');
 function popupOpened() {
     popup.classList.add("popup_opened")
     nameInput.value = nameInputNew.textContent;
@@ -11,12 +16,7 @@ function popupClosed() {
     popup.classList.remove("popup_opened")
 } 
 closeButton.addEventListener('click', popupClosed); 
-const formElement = document.querySelector('.popup__container');
 console.dir(formElement);
-const nameInput = document.querySelector('.popup__name');
-const jobInput = document.querySelector('.popup__job');
-const jobInputNew = document.querySelector('.profile__description');
-const nameInputNew = document.querySelector('.profile__name');
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
