889d70f8cb539154492407cfd8ed0aa6e0a1c066
Egor Sibirev <lay.12345@yandex.ru>
2022-05-22 12:44:04 +0300

    исправить ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,22 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-button');
-profileEditButton.addEventListener('click', function () {
-    popup.className = 'popup_opened';
-}); 
-closeButton.addEventListener('click', function () {
-    popup.className = 'popup';
-}); 
+function popupOpened() {
+    popup.classList.add("popup_opened")
+    nameInput.value = nameInputNew.textContent;
+    jobInput.value = jobInputNew.textContent;
+} 
+profileEditButton.addEventListener('click', popupOpened); 
+function popupClosed() {
+    popup.classList.remove("popup_opened")
+} 
+closeButton.addEventListener('click', popupClosed); 
 const formElement = document.querySelector('.popup__container');
 console.dir(formElement);
 const nameInput = document.querySelector('.popup__name');
 const jobInput = document.querySelector('.popup__job');
 const jobInputNew = document.querySelector('.profile__description');
 const nameInputNew = document.querySelector('.profile__name');
-nameInput.value = nameInputNew.textContent;
-jobInput.value = jobInputNew.textContent;
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
@@ -25,6 +27,6 @@ function formSubmitHandler (evt) {
     // Вставьте новые значения с помощью textContent
     jobInputNew.textContent = jobInput.value;
     nameInputNew.textContent = nameInput.value;
-    popup.className = 'popup';
+    popupClosed();
 }
 formElement.addEventListener('submit', formSubmitHandler);
--- scripts/script__like-button.js
+++
@@ -1,9 +0,0 @@
-let likeButton = document.querySelector('.element__like-button');
-likeButton.addEventListener('click', function () {
-    likeButton.classList.add('element__like-button_active');
-    likeButton.classList.remove('element__like-button');
-    console.log(likeButton);
-}); 
-
-
-
